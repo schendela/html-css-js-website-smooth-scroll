568ad600fc36668966077990f67474613bad8796
Schendela <adamschendel@gmail.com>
2023-05-08 21:07:49 -0500

    update

--- app.js
+++ app.js
@@ -54,3 +54,48 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
+// // Get the modal
+// var modal = document.getElementById("zivmodal");
+// var modal1 = document.getElementById("sevmodal");
+// // Get the button that opens the modal
+// var btn = document.getElementById("zivaroBtn");
+// var btn1 = document.getElementById("sev1Btn");
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+document.addEventListener('click', function (e) {
+  e = e || window.event;
+  var target = e.target || e.srcElement;
+
+  if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
+      if (target.hasAttribute('data-target')) {
+          var m_ID = target.getAttribute('data-target');
+          document.getElementById(m_ID).classList.add('open');
+          e.preventDefault();
+      }
+  }
+
+  // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+  if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+      var modal = document.querySelector('[class="modal open"]');
+      modal.classList.remove('open');
+      e.preventDefault();
+  }
+}, false);
